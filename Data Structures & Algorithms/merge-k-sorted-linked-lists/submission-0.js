8528dae851448c617711a0eb1328aa7b8d2bda9d
Derrick <dhouger@gmail.com>
2026-06-30 15:22:20 -0700

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.js
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} list1
+     * @param {ListNode} list2
+     * @return {ListNode}
+     */
+    mergeTwoLists(list1, list2) {
+        const dummy = new ListNode(0);
+        let tail = dummy;
+
+        let l1Curr = list1;
+        let l2Curr = list2;
+
+        while (l1Curr && l2Curr) {
+            if (l1Curr.val <= l2Curr.val) {
+                tail.next = l1Curr;
+                l1Curr = l1Curr.next;
+            } else {
+                tail.next = l2Curr;
+                l2Curr = l2Curr.next;
+            }
+
+            tail = tail.next;
+        }
+
+        tail.next = l1Curr || l2Curr;
+
+        return dummy.next;
+    }
+
+    /**
+     * @param {ListNode[]} lists
+     * @return {ListNode}
+     */
+    mergeKLists(lists) {
+        let merged = null;
+
+        for (let list of lists) {
+            merged = this.mergeTwoLists(merged, list);
+        }
+
+        return merged;
+    }
+}
